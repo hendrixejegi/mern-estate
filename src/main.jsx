377352b970ceedc67cc435ece334556f7c80971a
Hendrix Ejegi <tosanwumiae@gmail.com>
2026-05-02 16:25:22 +0100

    create pages and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './pages/home.jsx';
+import SignIn from './pages/sign-in.jsx';
+import SignUp from './pages/sign-up.jsx';
+import About from './pages/about.jsx';
+import Profile from './pages/profile.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>,
+);
