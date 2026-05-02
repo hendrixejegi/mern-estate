8109a8bec70661958f06734f725afb2c934e1b23
Hendrix Ejegi <tosanwumiae@gmail.com>
2026-05-02 19:50:53 +0100

    create server

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,15 +6,18 @@ import SignIn from './pages/sign-in.jsx';
 import SignUp from './pages/sign-up.jsx';
 import About from './pages/about.jsx';
 import Profile from './pages/profile.jsx';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+    <App>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </App>
   </BrowserRouter>,
 );
