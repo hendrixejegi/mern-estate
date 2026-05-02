a5c37a092258d492631f858a886de925be176847
Hendrix Ejegi <tosanwumiae@gmail.com>
2026-05-03 00:50:34 +0100

    create error middleware

--- server/index.js
+++ server/index.js
@@ -21,3 +21,10 @@ app.use('/api/auth', authRouter);
 app.listen(3000, () => {
   console.log('server is running at port 3000...');
 });
+
+// error handler middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({ success: false, message, statusCode });
+});
