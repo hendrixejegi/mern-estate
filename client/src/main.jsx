885aa7a01c52995fa8a5430717541fdcd59e39f3
Hendrix Ejegi <tosanwumiae@gmail.com>
2026-05-03 10:00:27 +0100

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,21 +7,24 @@ import SignUp from './pages/sign-up.jsx';
 import About from './pages/about.jsx';
 import Profile from './pages/profile.jsx';
 import App from './App.jsx';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </App>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </App>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 );
