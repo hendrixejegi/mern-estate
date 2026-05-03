9a1accb37c5244a37e7921887b93f24f15b60812
Hendrix Ejegi <tosanwumiae@gmail.com>
2026-05-03 14:47:33 +0100

    create user update api

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ await mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
@@ -26,5 +28,6 @@ app.listen(3000, () => {
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
+  console.log(err);
   res.status(statusCode).json({ success: false, message, statusCode });
 });
