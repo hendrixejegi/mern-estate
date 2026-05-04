ef5044eb2b0d87e7431a69081907f9a3f185205a
Hendrix Ejegi <tosanwumiae@gmail.com>
2026-05-04 11:49:56 +0100

    complete upload listing image functionality

--- client/src/pages/create-listing.jsx
+++ client/src/pages/create-listing.jsx
@@ -1,4 +1,79 @@
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from '@firebase/storage';
+import { useState } from 'react';
+import { app } from '../firebase';
+
 export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadErr, setImageUploadErr] = useState(false);
+  const [uploading, setUploading] = useState(false);
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload progress ${Math.round(progress)}`);
+        },
+        (error) => reject(error),
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) =>
+            resolve(downloadUrl),
+          );
+        },
+      );
+    });
+  };
+
+  const handleImageSubmit = () => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadErr(false);
+      const promises = [];
+
+      for (const file of files) {
+        promises.push(storeImage(file));
+      }
+
+      Promise.all(promises)
+        .then(
+          (urls) =>
+            setFormData({
+              ...formData,
+              imageUrls: formData.imageUrls.concat(urls),
+            }),
+          setImageUploadErr(false),
+        )
+        // eslint-disable-next-line no-unused-vars
+        .catch((error) =>
+          setImageUploadErr('Image upload failed (2 mb max per image'),
+        )
+        .finally(() => setUploading(false));
+    } else {
+      setImageUploadErr('You can only upload 6 images per listing');
+    }
+  };
+
+  const handleDeleteImage = (i) =>
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, j) => j !== i),
+    });
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -127,6 +202,7 @@ export default function CreateListing() {
             </div>
           </div>
         </div>
+        {/* Image upload section */}
         <div className="flex flex-col flex-1 gap-4">
           <p className="font-semibold">
             Images:{' '}
@@ -142,14 +218,40 @@ export default function CreateListing() {
               accept="image/*"
               multiple
               className="p-3 border border-gray-300 rounded w-full"
+              onChange={(e) => setFiles(e.target.files)}
             />
             <button
               type="button"
+              onClick={handleImageSubmit}
+              disabled={uploading}
               className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
             >
-              upload
+              {uploading ? 'Uploading...' : 'Upload'}
             </button>
           </div>
+          {imageUploadErr && (
+            <p className="text-red-700 text-sm">{imageUploadErr}</p>
+          )}
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, idx) => (
+              <div
+                key={url}
+                className="flex justify-between p-3 border items-center"
+              >
+                <img
+                  src={url}
+                  alt="listing image"
+                  className="size-20 object-contain rounded-lg"
+                />
+                <button
+                  onClick={() => handleDeleteImage(idx)}
+                  type="button"
+                  className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className="p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 bg-slate-700 text-white">
             create listing
           </button>
--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   getDownloadURL,
@@ -29,30 +29,28 @@ export default function Profile() {
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const dispatch = useDispatch();
 
-  const handleFileUpload = useCallback(
-    (file) => {
-      const storage = getStorage(app);
-      const fileName = new Date().getTime() + file.name;
-      const storageRef = ref(storage, fileName);
-      const uploadTask = uploadBytesResumable(storageRef, file);
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          setFilePerc(Math.round(progress));
-        },
-        () => setFileUploadErr(true),
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) =>
-            setFormData({ ...formData, avatar: downloadUrl }),
-          );
-        },
-      );
-    },
-    [formData],
-  );
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      // eslint-disable-next-line no-unused-vars
+      (err) => setFileUploadErr(true),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) =>
+          setFormData({ ...formData, avatar: downloadUrl }),
+        );
+      },
+    );
+  };
 
   useEffect(() => {
     if (file) {
